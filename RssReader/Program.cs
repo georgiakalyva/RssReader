ab14a35a4c2ad1374415dbf205230859a2f0010f
Georgia Kalyva <georgia_k22@hotmail.com>
2019-09-24 16:16:09 +0300

    Add files via upload

---
+++ RssReader/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ServiceModel.Syndication;
+using System.Xml;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RssReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://azurecomcdn.azureedge.net/en-in/blog/topics/cognitive-services/feed/";
+            XmlReader reader = XmlReader.Create(url);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            reader.Close();
+            foreach (SyndicationItem item in feed.Items)
+            {
+                String subject = item.Title.Text;
+                String summary = item.Summary.Text;
+                String link = item.Links.FirstOrDefault().Uri.ToString();
+                String date = item.PublishDate.ToString("dd/MM/yyyy");
+
+                Console.WriteLine("*"+subject+"*");
+                Console.WriteLine("    " + link);
+                Console.WriteLine("    " + date);
+                Console.WriteLine("    " + summary);
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
